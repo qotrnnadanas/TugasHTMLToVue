91a00fb6a9ae1a9045ac94d229f00aee84009876
qotrnnadanas <qotrunnafa@gmail.com>
2023-06-05 19:41:44 +0700

    fist commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <HelloQtrn msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import HelloQtrn from './components/HelloQtrn.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloQtrn
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
