175cb7571d70e5b789c157b0ae981b8196a77633
qotrnnadanas <qotrunnafa@gmail.com>
2023-06-08 18:51:29 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+import '@lottiefiles/lottie-player'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
 import { createApp } from 'vue'
 import App from './App.vue'
 
