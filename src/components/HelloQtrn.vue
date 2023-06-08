4a1e79c4d24e05e69a0e911b4de6a1bdc46d775c
qotrnnadanas <qotrunnafa@gmail.com>
2023-06-08 19:06:38 +0700

    update

--- src/components/HelloQtrn.vue
+++ src/components/HelloQtrn.vue
@@ -5,6 +5,7 @@
                <a class="navbar-brand" href="/">
                  <img class="logo" src="logo.png" alt="Logo">
                </a>
+               
                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                  aria-expanded="false" aria-label="Toggle navigation">
